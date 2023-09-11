a5961d0eef86ff295a4bfe4c1b55ad1ed0f9fcd9
ADMIN <ADMIN@LAB1-MAY32>
2023-09-11 15:56:20 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace csharp.bai8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtdl = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtal = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(319, 339);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(78, 36);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtdl
+            // 
+            this.txtdl.Location = new System.Drawing.Point(169, 114);
+            this.txtdl.Name = "txtdl";
+            this.txtdl.Size = new System.Drawing.Size(294, 20);
+            this.txtdl.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(240, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(223, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "tính can chi";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "năm dương lịch";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 248);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "năm âm lịch";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // txtal
+            // 
+            this.txtal.Location = new System.Drawing.Point(169, 241);
+            this.txtal.Name = "txtal";
+            this.txtal.Size = new System.Drawing.Size(294, 20);
+            this.txtal.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtal);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtdl);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtdl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtal;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace csharp.bai8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+        string can, chi;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            switch (Convert.ToInt32(txtdl.Text.Substring(3, 1))) {
+                case 0:
+                    can = "canh";
+                    break;
+                case 1:
+                    can = "tân";
+                    break;
+                case 2:
+                    can = "nhâm";
+                    break;
+                case 3:
+                    can = "quý";
+                    break;
+                case 4:
+                    can = "giáp";
+                    break;
+                case 5:
+                    can = "ất";
+                    break;
+                case 6:
+                    can = "bính";
+                    break;
+                case 7:
+                    can = "đinh";
+                    break;
+                case 8:
+                    can = "mậu";
+                    break;
+                case 9:
+                    can = "kỷ";
+                    break;
+
+            }
+            switch (Convert.ToInt32(txtdl.Text)  % 12) { 
+
+
+                case 0:
+                    chi = "thân";
+                    break;
+                case 1:
+                    chi = "dậu";
+                    break;
+                case 2:
+                    chi = "tuất";
+                    break;
+                case 3:
+                    chi = "hợi";
+                    break;
+                case 4:
+                    chi = "tý";
+                    break;
+
+                case 5:
+                    chi = "sửu";
+                    break;
+                case 6:
+                    chi = "mão";
+                    break;
+                case 7:
+                    chi = "thìn";
+                    break;
+                case 8:
+                    chi = "tị";
+                    break;
+                case 9:
+                    chi = "ngọ";
+                    break;
+                case 10:
+                    chi = "mùi";
+                    break;
+
+
+
+
+            }
+            txtal.Text = can + " " + chi;
+        }
+        }
+    }
+
+
